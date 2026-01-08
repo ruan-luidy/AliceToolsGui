de358135a53bdfe1af2962c8dc6a83e28c3e4d2d
ruan-luidy <125713061+ruan-luidy@users.noreply.github.com>
2026-01-08 16:06:37 -0300

    Add value converters and ViewModel locator resources
    Introduced Long2VisibilityConverter, String2VisibilityConverter, and Boolean2VisibilityConverter for UI binding. Added ViewModelLocator resource dictionary for mapping ViewModels to Views. Refactored MainWindow and AcxOperationsView XAML to improve structure, use HandyControl TabControl, and streamline button definitions.

---
+++ AliceToolsGui.Wpf/Converters/ValueConverters.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AliceToolsGui.Wpf.Converters;
+
+public class Long2VisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is int intValue)
+        {
+            return intValue > 0 ? Visibility.Visible : Visibility.Collapsed;
+        }
+        
+        if (value is long longValue)
+        {
+            return longValue > 0 ? Visibility.Visible : Visibility.Collapsed;
+        }
+        
+        return Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class String2VisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string str)
+        {
+            return string.IsNullOrEmpty(str) ? Visibility.Collapsed : Visibility.Visible;
+        }
+        
+        return Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Boolean2VisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool boolValue)
+        {
+            return boolValue ? Visibility.Visible : Visibility.Collapsed;
+        }
+        
+        return Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
